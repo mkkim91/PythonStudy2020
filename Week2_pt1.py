df8da96c26c0c72cf69a3e58dad455b930877be9
mkkim91 <mkminkyungkim@gmail.com>
2020-10-28 21:45:47 -0400

    Create Week2_pt1.py

---
+++ Week2_pt1.py
@@ -0,0 +1,32 @@
+# Week 2 of Study
+# dir(__builtins__) -> everything built in
+
+
+# fuctions 
+
+def samplefunc():
+  n = input ("some input")
+  print(n)
+
+
+# fn w/ args
+def add(n1, n2):
+  n3 = n1+n2
+  print(n3)
+ 
+ # I can make a eprate module by writing a file (i.e. myMath.py) and import myMath
+
+
+#strings 
+# string.lower(), string.upper(), strings are arrys printing upto certin string a[n1:n2]
+
+
+#lists (arrays)
+list1 = [2,3,4,510,102]
+
+#tuple is immutable 
+# tuple1 = (1,2,3,4,5)
+# I can convert tuples into lists and vice a versa 
+
+#Dictionaries - keys & values 
+# age = { 'min':33, 'jang':12, 'han':23}
